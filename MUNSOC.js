4c4aad1c6c0f8ffe85cdca17991c33519f90cec1
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-07-05 22:37:46 +0530

    nav bar fixed

--- MUNSOC.js
+++ MUNSOC.js
@@ -4,3 +4,19 @@ window.addEventListener('scroll', () => {
 
     text.style.marginTop = value * 0.9 + 'px';
 })
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
