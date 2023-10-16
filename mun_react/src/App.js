c2618120794c5196c24f7434681ca9c88b77232e
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-10-17 00:11:03 +0530

    text and logo addition

--- mun_react/src/App.js
+++ mun_react/src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className="back"></div>
-    
+    <div className='nav'>
+    <img class="rectangle-icon" alt="" src="rectangle-4@2x.png" />
+    </div>
+    <div className="back">
+    {/* <img class="backimg" alt="" src="back.png" /> */}
+      <div className='blurbox'></div>
+      <div className='headtext'>
+      <div class="model-united-nations">MODEL UNITED NATIONS</div>
+      <div class="nit-agartala">NIT AGARTALA</div>
+      </div>
+    </div>
     </>
   );
 }
