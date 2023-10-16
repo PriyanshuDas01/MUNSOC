e42474c912f76edf08479778dd63ba1bcc5d0a0f
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-10-17 00:49:12 +0530

    update

--- mun_react/src/App.js
+++ mun_react/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <img class="rectangle-icon" alt="" src="rectangle-4@2x.png" />
     </div>
     <div className="back">
-    {/* <img class="backimg" alt="" src="back.png" /> */}
+    <img class="backimg" alt="" src="back.png" />
       <div className='blurbox'></div>
       <div className='headtext'>
       <div class="model-united-nations">MODEL UNITED NATIONS</div>
