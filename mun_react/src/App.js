da5c92246eac926166acb0aeeaffbea9dea3f965
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-10-21 01:26:24 +0530

    navbar

--- mun_react/src/App.js
+++ mun_react/src/App.js
@@ -4,17 +4,38 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className='nav'>
-    <img class="rectangle-icon" alt="" src="rectangle-4@2x.png" />
-    </div>
-    <div className="back">
-    <img class="backimg" alt="" src="back.png" />
-      <div className='blurbox'></div>
-      <div className='headtext'>
-      <div class="model-united-nations">MODEL UNITED NATIONS</div>
-      <div class="nit-agartala">NIT AGARTALA</div>
+      <div className='n'>
       </div>
-    </div>
+      <header class="top">
+        <div class="nav">
+          <div className='logo'>
+            <img className="rectangle-icon" alt="" src="rectangle-4@2x.png" /></div>
+          <div className="menu">
+            <div class="home"><a href="!"><button class="button2">
+              Home
+            </button></a></div>
+            <div class="home"><a href="!"><button class="button2">
+              Commettiees
+            </button></a></div>
+            <div class="home"><a href="!"><button class="button2">
+              Contact Us
+            </button></a></div>
+            {/* <div class="home"><a href="!"><button class="button2">
+              Login
+            </button></a></div> */}
+          </div>
+        </div>
+      </header>
+
+      <div className="back">
+
+        <img className="backimg" alt="" src="back.png" />
+        <div className='blurbox'></div>
+        <div className='headtext'>
+          <div class="model-united-nations">MODEL UNITED NATIONS</div>
+          <div class="nit-agartala">NIT AGARTALA</div>
+        </div>
+      </div>
     </>
   );
 }
