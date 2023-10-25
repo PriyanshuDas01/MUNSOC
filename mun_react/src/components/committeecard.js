4209f3e6c06c2bd4e9efe55c6781b77596be73c0
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-10-26 02:39:30 +0530

    card

---
+++ mun_react/src/components/committeecard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './comcard.css'
+
+export default class About extends Component {
+
+    render() {
+        return (
+            <>
+            </>)}}
