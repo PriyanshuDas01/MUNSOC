9bba0a7ac2c70db4a6dff1826a2f19f0d30bd788
Priyanshu Das <priyanshudas01@gmail.com>
2023-10-26 02:54:02 +0530

    Committee cards
    All 4 committee cards were added

--- mun_react/src/components/committeecard.js
+++ mun_react/src/components/committeecard.js
@@ -1,9 +1,101 @@
 import React, { Component } from 'react'
 import './comcard.css'
 
-export default class About extends Component {
+export default class Card extends Component {
 
     render() {
         return (
             <>
+                <div class="com1">
+        <div class="circle" style="--clr:#028bf4;">
+            <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\disec.png" class="logo1"/>
+            <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\dicom.jpg" class="logo2"/>
+            <p class="agenda">
+                <b>Disarmament and International Security Committee (DISEC)</b>
+            </p>
+            <div class="agendatext">
+                The UN General Assembly (UNGA) is the main policy-making organ of the Organization. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations. Each of the Member States of the United Nations has an equal vote.
+                <div>
+                    <button class="button1">
+                       <b class="btext">EB & Agenda</b>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="comImg">
+             
+        </div>
+    </div>
+
+
+    <div class="com2">
+        <div class="circle2" style="--clr:#028bf4;">
+            <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\who.png" class="logo12"/>
+            <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\p2.jpg" class="logo22"/>
+            <p class="agenda2">
+                <b>World Health Organization (WHO)</b>
+            </p>
+            <div class="agendatext2">
+                The UN General Assembly (UNGA) is the main policy-making organ of the Organization. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations. Each of the Member States of the United Nations has an equal vote.
+                <div>
+                    <button class="button12">
+                       <b class="btext">EB & Agenda</b>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="comImg">
+             
+        </div>
+    </div>
+    
+
+        <><div class="com1">
+            <div class="circle" style="--clr:#028bf4;">
+                <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\legal.png" class="logo1" />
+                <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\p3.jpg" class="logo2" />
+                <p class="agenda">
+                    <b class="legal">UNGA Sixth Committee-Legal</b>
+                </p>
+                <div class="agendatext">
+                    The UN General Assembly (UNGA) is the main policy-making organ of the Organization. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations. Each of the Member States of the United Nations has an equal vote.
+                    <div>
+                        <button class="button1">
+                            <b class="btext">EB & Agenda</b>
+                        </button>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="comImg">
+
+            </div>
+        </div><div class="com2">
+                <div class="circle2" style="--clr:#028bf4;">
+                    <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\unhrc.png" class="logo12" />
+                    <img src="C:\Users\priya\OneDrive\Desktop\Web rotate\resoueces\p4.jpg" class="logo22" />
+                    <p class="agenda2">
+                        <b>United Nations Human Rights Council (UNHRC)</b>
+                    </p>
+                    <div class="agendatext2">
+                        The UN General Assembly (UNGA) is the main policy-making organ of the Organization. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations. Each of the Member States of the United Nations has an equal vote.
+                        <div>
+                            <button class="button12">
+                                <b class="btext">EB & Agenda</b>
+                            </button>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="comImg">
+
+                </div>
+            </div></>
+
             </>)}}
