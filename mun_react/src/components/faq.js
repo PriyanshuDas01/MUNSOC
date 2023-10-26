20c8399040102a0031ab38eb3dc17a94df39fe12
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-10-26 15:36:01 +0530

    template faq

---
+++ mun_react/src/components/faq.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './faq.css'
+
+export default class About extends Component {
+
+    render() {
+        return (
+            <>
+            </>)}}
