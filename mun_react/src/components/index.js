6c661f9bc747257e407f25149c392e17ebb05051
anshuman-rai-27 <anshumanrishi27@gmail.com>
2023-11-30 16:39:15 +0530

    video compression

--- mun_react/src/components/index.js
+++ mun_react/src/components/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './index.css'
 
 function Index() {
-    return (
+  return (
     <>
-    <div className="back">
-      <video className="vid" autoPlay muted loop src="back.mp4"/>
+      <div className="back">
+        <video className="vid" autoPlay muted loop src="back.webmhd.webm" />
 
-{/* <img className="backimg" alt="" src="back.png" /> */}
-{/* <div className='blurbox'></div> */}
-<div className='headtext'>
-  <div className="model-united-nations ">MODEL UNITED NATIONS</div>
-            <div className="nit-agartala">NIT AGARTALA</div>
-            <div className="buttonr"><button><a href="https://unstop.com/p/nita-mun-2024-munsoc-nit-agartala-832216">REGISTER</a></button></div>
-</div>
-</div>
+        {/* <img className="backimg" alt="" src="back.png" /> */}
+        {/* <div className='blurbox'></div> */}
+        <div className='headtext'>
+          <div className="model-united-nations ">MODEL UNITED NATIONS</div>
+          <div className="nit-agartala">NIT AGARTALA</div>
+          <div className="buttonr"><button><a href="https://unstop.com/p/nita-mun-2024-munsoc-nit-agartala-832216">REGISTER</a></button></div>
+        </div>
+      </div>
     </>
   )
 }
