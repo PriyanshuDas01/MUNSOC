ac400a55f3f749ce66f97b4553ba9454b256bfe5
Priyanshu Das <priyanshudas01@gmail.com>
2023-11-30 13:54:17 +0530

    agenda and eb buttons
    coming soon discription

--- mun_react/src/components/committeecard.js
+++ mun_react/src/components/committeecard.js
@@ -30,7 +30,7 @@ export default class Card extends Component {
             The United Nations General Assembly First Committee, also known as UNGA-DISEC, is crucial for global security. Comprising all UN member states, it addresses disarmament and international security issues.UNGA-DISEC plays a vital role in shaping policies to mitigate threats, prevent conflicts, and promote global stability.
                 <div className='buttons'>
                     <button className="button1">
-                       <b className="btext">EB & Agenda</b>
+                       <b className="btext">EB & Agenda: Coming soon</b>
                     </button>
                 </div>
             
@@ -54,7 +54,7 @@ export default class Card extends Component {
             <div className="agendatext2">The World Health Organization (WHO) is crucial for global health leadership. It sets international health standards, coordinates responses to pandemics, and provides vital health information. As a specialized agency of the UN, WHO plays a central role in safeguarding public health, preventing diseases, and promoting equitable access to healthcare worldwide.
                 <div className='buttons'>
                     <button className="button12">
-                       <b className="btext">EB & Agenda</b>
+                       <b className="btext">EB & Agenda: Coming soon</b>
                     </button>
                 </div>
             </div>
@@ -78,7 +78,7 @@ export default class Card extends Component {
                 The United Nations General Assembly Legal Committee (UNGA Legal) is crucial for shaping international law. It reviews legal issues, drafts conventions, and provides a forum for nations to discuss legal matters. UNGA Legal's decisions influence global legal frameworks, fostering cooperation and ensuring the rule of law in the international community.
                     <div className='buttons'>
                         <button className="button1">
-                            <b className="btext">EB & Agenda</b>
+                            <b className="btext">EB & Agenda: Coming soon</b>
                         </button>
                     </div>
                 </div>
@@ -99,7 +99,7 @@ export default class Card extends Component {
                     The United Nations Human Rights Council (UNHRC) plays a pivotal role in promoting and protecting human rights globally. It investigates human rights violations, proposes resolutions, and engages in dialogue with nations to address issues. As a key UN body, the UNHRC contributes significantly to the advancement of human rights and international cooperation.
                         <div className='buttons'>
                             <button className="button12">
-                                <b className="btext">EB & Agenda</b>
+                                <b className="btext">EB & Agenda: Coming soon</b>
                             </button>
                         </div>
                     </div>
